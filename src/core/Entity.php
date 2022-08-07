3384973bbc7312470fbc69461e570a81ae408215
B4rt <pouzar.bart@seznam.cz>
2022-08-07 19:38:51 +0200

    added method to dump the table

--- src/core/Entity.php
+++ src/core/Entity.php
@@ -9,7 +9,20 @@ abstract class Entity {
     protected function __construct(
 
         protected PDO $dbc,
-        protected string $tableName
+        protected string $table
+
     ) {}
+
+    public function findAll() {
+
+        $sql = 'SELECT * FROM '. $this->table;
+
+        $stmt = $this->dbc->prepare($sql);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+
+    }
 
 }
