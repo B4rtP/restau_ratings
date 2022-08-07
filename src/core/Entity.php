f937ff6b8341c66f89e34d754748cecc048f2108
B4rt <pouzar.bart@seznam.cz>
2022-08-08 00:09:48 +0200

    added search query

--- src/core/Entity.php
+++ src/core/Entity.php
@@ -6,6 +6,8 @@ use PDO;
 
 abstract class Entity {
 
+    protected string $joinedTable;
+
     protected function __construct(
 
         protected PDO $dbc,
@@ -19,6 +21,43 @@ abstract class Entity {
 
         $stmt = $this->dbc->prepare($sql);
 
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+
+    }
+
+    public function findAllFromMatches(array $columns, string|array $lookingFor) {
+
+        is_array($lookingFor) ?: $lookingFor = array($lookingFor);
+
+        $whereString = '';
+        $toBind = [];
+
+        foreach ($columns as $col) {
+
+            foreach ($lookingFor as $item) {
+
+                $whereString .= $col . " LIKE ? OR ";
+                
+                $toBind[] = '%'.$item.'%';
+
+            }
+        }
+
+        $sql = 'SELECT * FROM ' . $this->table . ' WHERE ' . rtrim($whereString, ' OR ');
+
+        $stmt = $this->dbc->prepare($sql);
+
+        $pos = 1;
+
+        foreach ($toBind as $val) {
+            
+            $stmt->bindValue($pos, $val);
+            $pos++;
+
+        }
+        
         $stmt->execute();
 
         return $stmt->fetchAll();
