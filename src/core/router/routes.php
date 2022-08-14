cea16d2fb60f1b037e0dcb44dc1a267f5cddfbf1
B4rt <pouzar.bart@seznam.cz>
2022-08-14 18:55:20 +0200

    .

--- src/core/router/routes.php
+++ src/core/router/routes.php
@@ -1,8 +1,26 @@
 <?php
 
+use Src\controllers\EntryController;
 use Src\core\router\Router;
 use Src\controllers\MainController;
+use Src\controllers\RestaurantController;
+use Src\controllers\ReviewController;
 
 Router::getInstance()
 
-->get('', [MainController::class => 'index']);
+->get('', [MainController::class => 'index'])
+->post('search', [MainController::class => 'searchRestaurants'])
+
+->get('restaurant', [RestaurantController::class => 'index'])
+
+->get('sign_in', [EntryController::class => 'login'])
+->get('sign_up', [EntryController::class => 'register'])
+->post('sign_up', [EntryController::class => 'registerSubmit'])
+->post('sign_in', [EntryController::class => 'loginSubmit'])
+->get('sign_out', [EntryController::class => 'logout'])
+
+->get('my_review', [ReviewController::class => 'index'])
+->post('submit_new_review', [ReviewController::class => 'submitNewReview'])
+->get('edit_my_review', [ReviewController::class => 'displayEditForm'])
+->post('edit_my_review', [ReviewController::class => 'updateReview'])
+->post('delete_review', [ReviewController::class => 'deleteReview']);
