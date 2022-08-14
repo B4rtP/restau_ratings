504ac623dc5d5bec96d9c303e64841973dbd30c3
B4rt <pouzar.bart@seznam.cz>
2022-08-14 18:45:01 +0200

    .

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Src\controllers\StatusController;
+use Src\core\Bootstrap;
 use Src\core\DB;
 use Src\core\router\Router;
 use Src\exceptions\RouteNotFoundException;
@@ -7,33 +9,18 @@ use Src\exceptions\RouteNotFoundException;
 session_start();
 
 define('ROOT_PATH', dirname(__DIR__). '/');
-define('CORE_PATH', ROOT_PATH . 'src/core/');
 define('VIEW_PATH', ROOT_PATH . 'src/views/');
 
 require_once ROOT_PATH . 'vendor/autoload.php';
-require_once CORE_PATH . 'router/routes.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+require_once ROOT_PATH . 'src/core/router/routes.php';
 
 try {
 
-    DB::getInstance()->connect($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
-
-} catch (PDOException $e) {
-
-    throw new PDOException($e->getMessage(), $e->getCode());
-}
-
-
-try {
-    
-    Router::getInstance()->resolve($_SERVER['REQUEST_METHOD'], $_GET['query'] ?? '');
+    $bootstrap = new Bootstrap(DB::getInstance(), Router::getInstance());
+    $bootstrap();
 
 } catch (RouteNotFoundException) {
 
-    // StatusController
+    (new StatusController())->runAction('pageNotFound');
 
 }
-
-
