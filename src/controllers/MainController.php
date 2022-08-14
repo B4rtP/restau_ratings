e64b2be017104a04d313544bdffc58241ed8d3ff
B4rt <pouzar.bart@seznam.cz>
2022-08-14 18:48:03 +0200

    added ability to search restaurants

--- src/controllers/MainController.php
+++ src/controllers/MainController.php
@@ -3,25 +3,63 @@
 namespace Src\controllers;
 
 use Src\core\Controller;
+use Src\core\Entity;
 use Src\core\View;
-use Src\entities\AnonymousEntity;
+use Src\enums\MatchMode;
 use Src\services\Formatter;
-use Src\services\helpers\DateTimeFormatter;
-use Src\services\helpers\PercentFormatter;
+use Src\services\helpers\PrettyDateTime;
+use Src\services\helpers\ToPercent;
 
 class MainController extends Controller {
 
+    private Entity $entity;
+
+    public function beforeAction() {
+
+        $this->entity = Entity::fromView($this->dbc,'restaurants_list');
+
+        return true;
+
+    }
+
     public function indexAction() {
 
-        $restaurants = AnonymousEntity::fromView($this->dbc, 'restaurants_list')->findAll();
+        $restaurants = $this->entity->findAll();
 
-        Formatter::convert($restaurants)->use(
+        $this->formatAndDisplay($restaurants);
+    }
 
-            new PercentFormatter('rev_average'),
-            new DateTimeFormatter('rev_newest')
+    public function searchRestaurantsAction() {
+
+        $input = $_POST['searchfield'];
+
+        $restaurants = $this->entity->findMatches([
+            'rr_name',
+            'rr_cuisine',
+            'rr_address'
+        ], explode(' ', $input), MatchMode::ANY);
+
+        if (! $restaurants) {
+
+            $_SESSION['message'] = 'Restaurants not found';
+
+            header('Location:/');
+            exit;
+        }
+
+        $this->formatAndDisplay($restaurants);
+
+    }
+
+    private function formatAndDisplay(array $restaurantObjs) {
+
+        Formatter::convert($restaurantObjs)->use(
+
+            new ToPercent('rev_average'),
+            new PrettyDateTime('rev_newest')
         );
-        
-        $data['restaurants'] = $restaurants;
+
+        $data['restaurants'] = $restaurantObjs;
 
         View::display('default', $data)->setLayout('main');
 
